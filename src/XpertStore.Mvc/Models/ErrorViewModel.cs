1fb24ff4ca9f512a62a20319486b58f3bd622afd
Victor Higaki <victorhigaki@outlook.com>
2025-03-17 21:42:45 -0300

    Add project files.

---
+++ src/XpertStore.Mvc/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace XpertStore.Mvc.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
