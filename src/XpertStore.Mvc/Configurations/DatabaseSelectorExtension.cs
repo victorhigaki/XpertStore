784c6541ee9b436c7f52795cb2e9e3f6dec5ee43
Victor Higaki <victorhigaki@outlook.com>
2025-03-20 17:55:29 -0300

    feat: ensure migration helpers

---
+++ src/XpertStore.Mvc/Configurations/DatabaseSelectorExtension.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using XpertStore.Mvc.Data;
+
+namespace XpertStore.Mvc.Configurations
+{
+    public static class DatabaseSelectorExtension
+    {
+        public static void AddDatabaseSelector(this WebApplicationBuilder builder)
+        {
+            if (builder.Environment.IsDevelopment())
+            {
+                var connectionString = builder.Configuration.GetConnectionString("DefaultConnectionLite") ?? throw new InvalidOperationException("Connection string 'DefaultConnection' not found.");
+                builder.Services.AddDbContext<ApplicationDbContext>(options =>
+                    options.UseSqlite(connectionString));
+                builder.Services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = true)
+                    .AddEntityFrameworkStores<ApplicationDbContext>();
+            }
+            else
+            {
+                var connectionString = builder.Configuration.GetConnectionString("DefaultConnection") ?? throw new InvalidOperationException("Connection string 'DefaultConnection' not found.");
+                builder.Services.AddDbContext<ApplicationDbContext>(options =>
+                    options.UseSqlServer(connectionString));
+                builder.Services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = true)
+                    .AddEntityFrameworkStores<ApplicationDbContext>();
+            }
+        }
+    }
+}
