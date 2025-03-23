5cd5ccd6158190d6b7e2a9f16131fcd8e849dff1
Victor Higaki <victorhigaki@outlook.com>
2025-03-23 16:22:43 -0300

    feat: Produtos Image Upload

--- src/XpertStore.Mvc/Models/ProdutoViewModel.cs
+++ src/XpertStore.Mvc/Models/ProdutoViewModel.cs
@@ -1,11 +1,20 @@
-﻿namespace XpertStore.Mvc.Models;
+﻿using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel;
+
+namespace XpertStore.Mvc.Models;
 
 public class ProdutoViewModel
 {
     public Guid Id { get; set; }
     public string Nome { get; set; }
     public string Descricao { get; set; }
-    public string Imagem { get; set; }
+
+    public string? Imagem { get; set; }
+
+    [NotMapped]
+    [DisplayName("Imagem do Produto")]
+    public IFormFile? ImagemUpload { get; set; }
+
     public decimal Preco { get; set; }
     public bool Estoque { get; set; }
     public Guid CategoriaId { get; set; }
