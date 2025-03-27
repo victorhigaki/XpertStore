ca751e253504e6c57cdf8425c44aaea9e65c0821
Victor Higaki <victorhigaki@outlook.com>
2025-03-27 20:17:10 -0300

    feat: Add API Project

---
+++ src/XpertStore.Api/Controllers/Base/BaseCrudController.cs
@@ -0,0 +1,141 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using XpertStore.Data.Data;
+using XpertStore.Domain.Entities.Base;
+
+namespace XpertStore.Api.Controllers.Base;
+
+public class BaseCrudController<T> : BaseApiController where T : BaseEntity
+{
+    private readonly ApplicationDbContext _context;
+
+    public BaseCrudController(ApplicationDbContext context)
+    {
+        _context = context;
+    }
+
+    [HttpGet]
+    [ProducesResponseType(StatusCodes.Status200OK)]
+    [ProducesResponseType(StatusCodes.Status404NotFound)]
+    [ProducesDefaultResponseType]
+    public async Task<ActionResult<IEnumerable<T>>> GetProduto()
+    {
+        if (_context.Set<T>() == null)
+        {
+            return NotFound();
+        }
+
+        return await _context.Set<T>().ToListAsync();
+    }
+
+    [HttpGet("{id:int}")]
+    [ProducesResponseType(StatusCodes.Status200OK)]
+    [ProducesResponseType(StatusCodes.Status404NotFound)]
+    [ProducesDefaultResponseType]
+    public async Task<ActionResult<T>> GetProdutos(int id)
+    {
+        if (_context.Set<T>() == null)
+        {
+            return NotFound();
+        }
+
+        var produto = await _context.Set<T>().FindAsync(id);
+
+        if (produto == null)
+        {
+            return NotFound();
+        }
+
+        return Ok(produto);
+    }
+
+    [HttpPost]
+    [ProducesResponseType(StatusCodes.Status201Created)]
+    [ProducesResponseType(StatusCodes.Status400BadRequest)]
+    [ProducesDefaultResponseType]
+    public async Task<ActionResult<T>> PostProduto(T entity)
+    {
+        if (_context.Set<T>() == null)
+        {
+            return Problem("Erro ao criar um produto, contate o suporte!");
+        }
+
+        if (!ModelState.IsValid)
+        {
+            //return BadRequest(ModelState);
+
+            //return ValidationProblem(ModelState);
+
+            return ValidationProblem(new ValidationProblemDetails(ModelState)
+            {
+                Title = "Um ou mais erros de validação ocorreram!"
+            });
+        }
+
+        _context.Set<T>().Add(entity);
+        await _context.SaveChangesAsync();
+
+        return CreatedAtAction(nameof(GetProduto), new { id = entity.Id }, entity);
+    }
+
+    [HttpPut("{id:int}")]
+    [ProducesResponseType(StatusCodes.Status204NoContent)]
+    [ProducesResponseType(StatusCodes.Status400BadRequest)]
+    [ProducesResponseType(StatusCodes.Status404NotFound)]
+    [ProducesDefaultResponseType]
+    public async Task<IActionResult> Put(Guid id, T entity)
+    {
+        if (id != entity.Id) return BadRequest();
+
+        if (!ModelState.IsValid) return ValidationProblem(ModelState);
+
+        _context.Entry(entity).State = EntityState.Modified;
+
+        try
+        {
+            await _context.SaveChangesAsync();
+        }
+        catch (DbUpdateConcurrencyException)
+        {
+            if (!EntityExists(id))
+            {
+                return NotFound();
+            }
+            else
+            {
+                throw;
+            }
+        }
+
+        return NoContent();
+    }
+
+    [HttpDelete("{id:int}")]
+    [ProducesResponseType(StatusCodes.Status204NoContent)]
+    [ProducesResponseType(StatusCodes.Status404NotFound)]
+    [ProducesDefaultResponseType]
+    public async Task<IActionResult> Delete(int id)
+    {
+        if (_context.Produtos == null)
+        {
+            return NotFound();
+        }
+
+        var produto = await _context.Produtos.FindAsync(id);
+
+        if (produto == null)
+        {
+            return NotFound();
+        }
+
+        _context.Produtos.Remove(produto);
+        await _context.SaveChangesAsync();
+
+        return NoContent();
+    }
+
+    private bool EntityExists(Guid id)
+    {
+        return (_context.Set<T>()?.Any(e => e.Id == id)).GetValueOrDefault();
+    }
+}
