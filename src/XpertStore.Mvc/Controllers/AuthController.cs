a0785e248b66e8bf18e436b2f793836462b0cb06
Victor Higaki <victorhigaki@outlook.com>
2025-03-27 22:10:28 -0300

    feat: Registrar Form e Add Vendedor

---
+++ src/XpertStore.Mvc/Controllers/AuthController.cs
@@ -0,0 +1,61 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Options;
+using XpertStore.Entities.Models;
+using XpertStore.Mvc.Data;
+using XpertStore.Mvc.Models;
+
+namespace XpertStore.Mvc.Controllers;
+
+public class AuthController : Controller
+{
+    private readonly UserManager<IdentityUser> _userManager;
+    private readonly SignInManager<IdentityUser> _signInManager;
+    private readonly ApplicationDbContext _context;
+
+    public AuthController(
+        UserManager<IdentityUser> userManager,
+        SignInManager<IdentityUser> signInManager,
+        ApplicationDbContext context)
+    {
+        _userManager = userManager;
+        _signInManager = signInManager;
+        _context = context;
+    }
+
+    public IActionResult Registrar()
+    {
+        return View();
+    }
+
+    [HttpPost]
+    public async Task<IActionResult> Registrar([Bind("Email,Password,ConfirmPassword")] RegistrarUsuarioViewModel registrarUsuario)
+    {
+        if (!ModelState.IsValid) return ValidationProblem(ModelState);
+
+        var user = new IdentityUser
+        {
+            UserName = registrarUsuario.Email,
+            Email = registrarUsuario.Email,
+            EmailConfirmed = true
+        };
+
+        var result = await _userManager.CreateAsync(user, registrarUsuario.Password);
+
+        if (result.Succeeded)
+        {
+            await AddVendedor(user);
+            await _signInManager.SignInAsync(user, isPersistent: false);
+            return LocalRedirect(Url.Content("~/"));
+        }
+
+        return View(registrarUsuario);
+    }
+
+    private async Task AddVendedor(IdentityUser user)
+    {
+        var userId = await _userManager.GetUserIdAsync(user);
+        await _context.Vendedor.AddAsync(new Vendedor { Id = new Guid(userId) });
+        await _context.SaveChangesAsync();
+    }
+}
---
+++ src/XpertStore.Mvc/Models/RegistrarUsuarioViewModel.cs
@@ -0,0 +1,21 @@
+﻿using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+
+namespace XpertStore.Mvc.Models;
+
+public class RegistrarUsuarioViewModel
+{
+    [Required(ErrorMessage = "O campo {0} é obrigatório")]
+    [EmailAddress(ErrorMessage = "O campo {0} está em formato inválido")]
+    [DisplayName("E-mail")]
+    public string Email { get; set; }
+
+    [Required(ErrorMessage = "O campo {0} é obrigatório")]
+    [StringLength(100, ErrorMessage = "O campo {0} precisa ter entre {2} e {1} caracteres", MinimumLength = 6)]
+    [DisplayName("Senha")]
+    public string Password { get; set; }
+
+    [Compare("Password", ErrorMessage = "As senhas não conferem")]
+    [DisplayName("Confirmar Senha")]
+    public string ConfirmPassword { get; set; }
+}
