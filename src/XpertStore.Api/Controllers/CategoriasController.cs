222929bef770f5447e3a4d806806dc71fb21afc2
Victor Higaki <victorhigaki@outlook.com>
2025-03-28 00:48:11 -0300

    feat: Categoria Delete validação Produto em uso

--- src/XpertStore.Api/Controllers/CategoriasController.cs
+++ src/XpertStore.Api/Controllers/CategoriasController.cs
@@ -1,4 +1,6 @@
-﻿using XpertStore.Api.Controllers.Base;
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using XpertStore.Api.Controllers.Base;
 using XpertStore.Data.Data;
 using XpertStore.Entities.Models;
 
@@ -6,7 +8,44 @@ namespace XpertStore.Api.Controllers;
 
 public class CategoriasController : BaseCrudController<Categoria>
 {
+    private readonly ApplicationDbContext _context;
+
     public CategoriasController(ApplicationDbContext context) : base(context)
     {
+        _context = context;
+    }
+
+    [HttpDelete("{id:Guid}")]
+    [ProducesResponseType(StatusCodes.Status204NoContent)]
+    [ProducesResponseType(StatusCodes.Status404NotFound)]
+    [ProducesDefaultResponseType]
+    public async Task<IActionResult> Delete(Guid id)
+    {
+        if (_context.Categorias == null)
+        {
+            return NotFound();
+        }
+
+        var categoria = await _context.Categorias.FindAsync(id);
+
+        if (categoria == null)
+        {
+            return NotFound();
+        }
+
+        if (await CategoriaEmUso(id))
+        {
+            return Problem("Categoria com Produto associado");
+        }
+
+        _context.Categorias.Remove(categoria);
+        await _context.SaveChangesAsync();
+
+        return NoContent();
+    }
+
+    private async Task<bool> CategoriaEmUso(Guid categoriaId)
+    {
+        return await _context.Produtos.AnyAsync(p => p.CategoriaId == categoriaId);
     }
 }
