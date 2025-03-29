4d36138072ac777be5d00969e937cb223c3a53e9
Victor Higaki <victorhigaki@outlook.com>
2025-03-29 11:19:38 -0300

    chore: move Swagger Configuration

--- src/XpertStore.Api/Program.cs
+++ src/XpertStore.Api/Program.cs
@@ -1,4 +1,4 @@
-using Microsoft.OpenApi.Models;
+using XpertStore.Api.Configuration;
 using XpertStore.Data.Configurations;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -6,36 +6,8 @@ var builder = WebApplication.CreateBuilder(args);
 builder.AddDatabaseSelector();
 
 builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
-builder.Services.AddEndpointsApiExplorer();
 
-builder.Services.AddSwaggerGen(c =>
-{
-    c.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme
-    {
-        Description = "Insira o token JWT desta maneira: Bearer {seu token}",
-        Name = "Authorization",
-        Scheme = "Bearer",
-        BearerFormat = "JWT",
-        In = ParameterLocation.Header,
-        Type = SecuritySchemeType.ApiKey
-    });
-
-    c.AddSecurityRequirement(new OpenApiSecurityRequirement
-    {
-        {
-            new OpenApiSecurityScheme
-            {
-                Reference = new OpenApiReference
-                {
-                    Type = ReferenceType.SecurityScheme,
-                    Id = "Bearer"
-                }
-            },
-            new string[] {}
-        }
-    });
-});
+builder.AddSwaggerConfig();
 
 var app = builder.Build();
 
