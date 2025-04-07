fa72c62b3da227c569b04f755142f7208d6b1627
Victor Higaki <victorhigaki@outlook.com>
2025-04-07 16:34:30 -0300

    feat: Produto Update Validations

--- src/XpertStore.Entities/Models/Produto.cs
+++ src/XpertStore.Entities/Models/Produto.cs
@@ -1,12 +1,31 @@
-﻿namespace XpertStore.Entities.Models;
+﻿using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+
+namespace XpertStore.Entities.Models;
 
 public class Produto
 {
+    [Key]
     public Guid Id { get; set; }
+
+    [Required(ErrorMessage = "Preencha o campo {0}!")]
     public string Nome { get; set; }
+
+
+    [Required(ErrorMessage = "Preencha o campo {0}!")]
+    [DisplayName("Descrição")]
     public string Descricao { get; set; }
-    public string Imagem { get; set; }
+
+    [Required(ErrorMessage = "Preencha o campo {0}!")]
+    public string? Imagem { get; set; }
+
+    [DisplayName("Preço")]
+    [Required(ErrorMessage = "Preencha o campo {0}!")]
+    [Range(0.01, Double.MaxValue, ErrorMessage = "{0} precisa ser maior que {1}.")]
     public decimal Preco { get; set; }
+
+    [Required(ErrorMessage = "Preencha o campo {0}!")]
+    [Range(0, int.MaxValue, ErrorMessage = "{0} precisa ser maior que {1}.")]
     public int Estoque { get; set; }
 
     public Categoria Categoria { get; set; }
